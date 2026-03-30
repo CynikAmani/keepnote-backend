88fda67e91040ebc3098e78712b461f18cd13cda
Cynik Amani <cynikpaul619@gmail.com>
2026-03-30 12:41:41 +0200

    fix: add AuthorizesRequests trait to base controller
    Add missing AuthorizesRequests trait to the base Controller class
    to restore authorization functionality. Without this trait, all
    authorization checks ($this->authorize()) in controllers fail
    with "Call to undefined method" errors.
    
    This affects:
    - TodoItemController (update, delete, toggleCompletion, updatePosition)
    - NoteController
    - TodoGroupController
    - LabelController
    - Any other controllers using authorization
    
    Also includes ValidatesRequests trait for consistent controller behavior.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 
 class Controller
 {
+    use AuthorizesRequests, ValidatesRequests;
+    
     //
 }
